b6c686937fe50786bee6e88250ec0065ef376513
Shadman <shadman.alizada@gmail.com>
2024-12-29 23:22:37 +0400

    feat: use KeyboardControls

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import { Canvas } from '@react-three/fiber';
-import { CameraType } from './interfaces/camera.interfaces';
 import Experience from './components/Experience';
-
-const cameraSetting: CameraType = {
-  fov: 45,
-  near: 0.1,
-  far: 200,
-  position: [2.5, 4, 6],
-};
+import { cameraSetting, keyboardMap } from './data/constants';
+import { KeyboardControls } from '@react-three/drei';
 
 function App() {
   return (
-    <Canvas shadows camera={cameraSetting}>
-      <Experience />
-    </Canvas>
+    <KeyboardControls map={keyboardMap}>
+      <Canvas shadows camera={cameraSetting}>
+        <Experience />
+      </Canvas>
+    </KeyboardControls>
   );
 }
 
--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,7 +1,23 @@
 import TrapAxe from '../components/Traps/TrapAxe';
 import TrapLimbo from '../components/Traps/TrapLimbo';
 import TrapSpinner from '../components/Traps/TrapSpinner';
+import { CameraType } from '../interfaces/camera.interfaces';
 
 export const blockCount = 5;
 
 export const blockTypes = [TrapSpinner, TrapLimbo, TrapAxe];
+
+export const cameraSetting: CameraType = {
+  fov: 45,
+  near: 0.1,
+  far: 200,
+  position: [2.5, 4, 6],
+};
+
+export const keyboardMap = [
+  { name: 'forward', keys: ['ArrowUp', 'KeyW'] },
+  { name: 'backward', keys: ['ArrowDown', 'KeyS'] },
+  { name: 'leftward', keys: ['ArrowLeft', 'KeyA'] },
+  { name: 'rightward', keys: ['ArrowRight', 'KeyD'] },
+  { name: 'jump', keys: ['Space'] },
+];
