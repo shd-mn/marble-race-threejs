4312f92fbe325db214be97e29a89479c862de16a
Shadman <shadman.alizada@gmail.com>
2025-02-19 23:39:26 +0400

    feat: add interface to game

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Canvas } from '@react-three/fiber';
 import Experience from './components/Experience';
 import { cameraSetting, keyboardMap } from './data/constants';
 import { KeyboardControls } from '@react-three/drei';
+import Interface from './components/Interface';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Canvas shadows camera={cameraSetting}>
         <Experience />
       </Canvas>
+      <Interface />
     </KeyboardControls>
   );
 }
